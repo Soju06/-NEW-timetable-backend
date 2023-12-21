33cedf49a434e7dbbda79e3caadfe455235967d6
leeyeonwoo <rexrex1105@gmail.com>
2023-12-21 11:54:35 +0900

    check auth funciton added

--- app/tools.py
+++ app/tools.py
@@ -21,6 +21,17 @@ def encToken(user_id):
   token = jwt.encode(tmp, SECRET, algorithm="HS256")
   return token
 
+def check_auth(token):
+  unix_time = int(time())
+  try:
+    tmp = jwt.decode(token, SECRET, algorithms="HS256")
+    if tmp["type"] == "auth" and tmp["end"] > unix_time:
+      return tmp["id"]
+    else:
+      return False
+  except:
+    return False
+
 def hashing_pw(plain_pw):
   
   salted_pw = plain_pw + SECRET
